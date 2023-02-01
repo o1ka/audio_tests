afb4910ba779be545b5e473726ece212d2c00796
o1ka <46780807+o1ka@users.noreply.github.com>
2023-02-01 14:18:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
 
 'use strict';
 
@@ -21,12 +14,9 @@ function onLoad() {
   audio = document.getElementById('gum-local');
   audio_no_aec = document.getElementById('gum-local-noaec');
   audio_select_ = document.getElementById("audiodevice");
-//  audio_select_myaudio = document.getElementById("audiodevice_myaudio");
-//  video = document.getElementById("video");
-//  myaudio = document.getElementById("myaudio");
   sink_label_ = document.getElementById("sinklabel");
- // sink_label_myaudio = document.getElementById("sinklabel_myaudio");
-  //video.play();
+  mic_label = document.getElementById("miclabel");
+
 
   audio_select_.onchange = async => {
     deviceSelected(audioContext, audio_select_, sink_label_)
@@ -34,21 +24,6 @@ function onLoad() {
 
   navigator.mediaDevices.getUserMedia( {audio: true, video: false} ).then(handleSuccess).catch(handleError);
   navigator.mediaDevices.getUserMedia( {audio: { echoCancellation: false }, video: false} ).then(handleSuccessNoAec).catch(handleError);
-
-
-/*
-  navigator.webkitGetUserMedia(
-      {audio:true},
-      function(stream) {
-        refreshDeviceList(true,audio_select_);
-   //     refreshDeviceList(true,audio_select_myaudio);
-        THESTREAM=stream;
-        stream.getAudioTracks()[0].stop();
-        console.log("stream closed");
-      },
-      function(err) {alert(err);}
-  );
-*/
 }
 
 function handleSuccess(stream) {
@@ -90,6 +65,8 @@ function handleError(error) {
 
 function refreshDeviceList(selectFirst, audio_select, sink_label) {
   navigator.mediaDevices.enumerateDevices().then( function(infos) {
+    const defaultAudioInput = infos.filter((infos) => infos.kind == "audioinput" && infos.deviceId == "default");
+    mic_label.innerHTML = 'Microphne: ' + defaultAudioInput.label;
     var count = 0;
     var curValue = audio_select.options[audio_select.selectedIndex].value;
     var curValueFound = false;
@@ -146,71 +123,3 @@ function deviceIdToSinkId(deviceId) {
 
 
 
-/*
-
-
-changeOutputButton.onclick = async () => {
-  const sinkId = deviceIdToSinkId(devicesDropdown.value);
-  await audioContext.setSinkId(sinkId);
-  if ('getSinkId' in audioContext) {
-  console.log(`audioContext.getSinkId() = ${JSON.stringify(audioContext.getSinkId())}`);
-  } else {
-  console.log(`audioContext.sinkId = ${JSON.stringify(audioContext.sinkId)}`);
-  }
-};
-
-getAudioOutputsButton.onclick = async () => {
-  console.log("getAudioOutputsButton");
-
-  try {
-    // More audio outputs are available when user grants access to the microphone.
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    stream.getTracks().forEach((track) => track.stop());
-  } finally {
-    populateDropdown();
-    if (!audioContext) playSound();
-  }
-};
-
-async function populateDropdown() {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const audioOutputs = devices.filter((device) => device.kind == "audiooutput");
-  console.log("populateDropdown");
-
-  devicesDropdown.innerHTML =
-    audioOutputs.map((device) => createOption(device)).join("") +
-    createOption({ deviceId: "silent", label: "No output" });
-  devicesDropdown.disabled = false;
-  changeOutputButton.disabled = false;
-}
-
-function playSound() {
-  audioContext = new AudioContext();
-  const osc = new OscillatorNode(audioContext);
-  const amp = new GainNode(audioContext, { gain: 0.03 });
-  osc.connect(amp).connect(audioContext.destination);
-  osc.start();
-  console.log("Playing sound...");
-}
-
-// Utils
-
-function deviceIdToSinkId(deviceId) {
-  if (deviceId === "default") return "";
-  if (deviceId === "silent") return { type: "none" };
-  return deviceId;
-}
-
-
-function log(text) {
-  logs.textContent += `${text}\r\n`;
-}
-
-
-function createOption({ deviceId, label }) {
-  return `<option value="${deviceId}">${label || "Default"}</option>`;
-}
-
-});
-
-*/
